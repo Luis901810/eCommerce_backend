94d2babb5ed4ba285acf94c6cc2c52ee5e0a6e09
Luis Mesajil <lhmesajil@gmail.com>
2023-12-08 20:43:55 -0500

    Add user model

---
+++ src/models/User.js
@@ -0,0 +1,69 @@
+const { DataTypes } = require('sequelize');
+const { phoneNumberRegex } = require('../utils/User/validators');
+
+module.exports = (sequelize) => {
+    sequelize.define('user', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: 'El formato del correo electrónico no es válido'
+                }
+            }
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                is: phoneNumberRegex,
+                msg: `El formato del número de teléfono no es válido. Se esperaba ${phoneNumberRegex}`
+            }
+        },
+        isEmailVerified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        isPhoneNumberVerified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        birthDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+            validate: {
+                isDate: true,
+                isBefore: new Date().toISOString().split('T')[0],
+            },
+        },
+        profilePicture: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUrl: {
+                    msg: 'El enlace de la imagen de perfil no es válido',
+                },
+            },
+        },
+        twoFactorEnabled: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+    })
+}
