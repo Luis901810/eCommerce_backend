12bfe84cbc18958ec6878aa437aee009a9d66b90
unknown <luis901810@gmail.com>
2023-12-12 19:11:32 -0500

    subo modificacion

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
+const pg = require("pg")
 // const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
 
 // const sequelize = new Sequelize(
