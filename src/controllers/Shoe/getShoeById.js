ff5737bcac902d1bd775473afa7a480571213b57
GreeTeethSkull <angelitorios366@gmail.com>
2023-12-14 23:40:37 -0500

    update getshoebyid controller

--- src/controllers/Shoe/getShoeById.js
+++ src/controllers/Shoe/getShoeById.js
@@ -1,11 +1,11 @@
-const { Shoe, ShoeCategory } = require("../../db")
+const { Shoe, ShoeCategory , ShoeSize , ShoeBrand , ShoeColor , ShoeGender } = require("../../db")
 
 const getShoeById = async (req, res) => {
 
     try {
         const { id } = req.params
         
-        const shoe = await Shoe.findByPk(id, {include:ShoeCategory})
+        const shoe = await Shoe.findByPk(id, {include:[{model:ShoeCategory},{model:ShoeSize},{model:ShoeBrand},{model:ShoeColor},{model:ShoeGender}]})
 
         if (!shoe)
             return res.status(404).json({ error: "Zapato no encontrado" });
