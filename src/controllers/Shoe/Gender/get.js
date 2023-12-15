d9a650f2f96a861543b9583b5a829800b42ec05f
Kevin_Padilla <kevin0696s@gmail.com>
2023-12-14 20:17:39 -0400

    Enpoint Get of all shoe's attributes maded

---
+++ src/controllers/Shoe/Gender/get.js
@@ -0,0 +1,15 @@
+const { ShoeGender } = require('../../../db')
+
+const get = async(req, res) => {
+    
+    try{
+        
+        const data = await ShoeGender.findAll()
+        return res.status(200).json(data)
+    } catch(error){
+        console.error(error);
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+module.exports = get;
