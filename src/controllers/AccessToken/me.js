4c5d4cb3063c4bb5b3db4709600eb07a88aeba42
Luis Mesajil <lhmesajil@gmail.com>
2023-12-18 20:06:27 -0500

    Add Routes: Login, Me, VerifyToken

--- src/controllers/AccessToken/me.js
+++ src/controllers/AccessToken/me.js
@@ -1,13 +1,12 @@
 const { User } = require('../../db')
-const jwt = require('jsonwebtoken')
 
 module.exports = async (req, res) => {
     try {
-        const token = req.header('x-access-token')
-        if (!token) return res.status(401).json({ error: 'Unauthorized' })
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findByPk(decoded.id)
+        // Get user
+        const user = await User.findByPk(req.userId)
         if (!user) return res.status(401).json({ error: 'El usuario no fue encontrado' })
+
+        // Return Response
         return res.json(user)
     } catch (error) {
         console.log(error)
---
+++ src/controllers/AccessToken/verifyToken.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = async (req, res, next) => {
+    const token = req.header('x-access-token')
+    if (!token) return res.status(401).json({ error: 'Unauthorized' })
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.userId = decoded.id
+    next()
+}
