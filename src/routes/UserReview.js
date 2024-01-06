2594f272c5045f84df3fbbfec11afca8a1f1144e
Luis Mesajil <lhmesajil@gmail.com>
2024-01-06 15:12:17 -0500

    ADD getReviews ctrl

--- src/routes/UserReview.js
+++ src/routes/UserReview.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 
+router.get('/', require('../controllers/UserReview/getReviews'))
 router.post('/', require('../controllers/UserReview/createReview'))
 
 module.exports = router
