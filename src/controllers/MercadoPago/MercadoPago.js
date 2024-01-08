e2c0831266dfcea7624eacffa66595f7648fc00a
Luis Mesajil <lhmesajil@gmail.com>
2024-01-08 15:07:22 -0500

    UPDATE MercadoPago to use Env var URL_FRONT

--- src/controllers/MercadoPago/MercadoPago.js
+++ src/controllers/MercadoPago/MercadoPago.js
@@ -3,7 +3,7 @@ const mercadopago = require('mercadopago')
 //!Credenciales del .env
 const ACCESS_TOKEN =
     'TEST-7120791930180798-112715-b8bed09589c90708ab8b212544f746b9-1555519585'
-const LOCALHOST = 'https://storecalzado.vercel.app/' //Del front
+const URL_FRONT = process.env.URL_FRONT || 'http://localhost:3000/'
 
 mercadopago.configure({
     access_token: ACCESS_TOKEN || '',
@@ -31,9 +31,9 @@ module.exports = async (req, res) => {
             items: ProductsOrder,
 
             back_urls: {
-                success: `${LOCALHOST}Successes`,
+                success: `${URL_FRONT}Successes`,
                 //Retorna esto si está mal
-                failure: `${LOCALHOST}Failures`,
+                failure: `${URL_FRONT}Failures`,
             },
 
             auto_return: 'approved',
