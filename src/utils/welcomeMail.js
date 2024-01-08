a74062e4fcdaaf0122bbcdb21ffa1c78a7bc2fc8
Sebastian Kang <sebi0522@gmail.com>
2024-01-08 11:19:11 -0300

    si no tiene nombre el usuario creado no se lo incluye en el mail

--- src/utils/welcomeMail.js
+++ src/utils/welcomeMail.js
@@ -1,9 +1,17 @@
-const generateWelcomeMessage = userName => `
-    <div style="font-family: Arial, sans-serif; background-color: #f5f5f5; padding: 20px; border-radius: 10px; max-width: 600px; margin: 0 auto;">
-        <h1 style="color: #333;">Bienvenido a nuestro sitio. Gracias por registrarte, ${userName}!</h1>
-        <p style="color: #555; font-size: 16px;">Gracias por registrarte en nuestro increíble sitio. Estamos emocionados de tenerte con nosotros.</p>
-        <p style="color: #555; font-size: 16px;">¡Esperamos que disfrutes de tu tiempo en nuestra plataforma!</p>
-    </div>
-`
+const generateWelcomeMessage = name => {
+    const welcomeMessage = `
+        <div style="font-family: Arial, sans-serif; background-color: #f5f5f5; padding: 20px; border-radius: 10px; max-width: 600px; margin: 0 auto;">
+            <h1 style="color: #333;">Bienvenido a nuestro sitio. ${
+                name
+                    ? `Gracias por registrarte, ${name}!`
+                    : 'Gracias por registrarte!'
+            }</h1>
+            <p style="color: #555; font-size: 16px;">Gracias por registrarte en nuestro increíble sitio. Estamos emocionados de tenerte con nosotros.</p>
+            <p style="color: #555; font-size: 16px;">¡Esperamos que disfrutes de tu tiempo en nuestra plataforma!</p>
+        </div>
+    `
+
+    return welcomeMessage
+}
 
 module.exports = { generateWelcomeMessage }
