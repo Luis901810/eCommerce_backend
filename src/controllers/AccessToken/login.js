3986426ceeee2c3f1a692216f76e07bcfa542e38
Luis Mesajil <lhmesajil@gmail.com>
2024-01-09 13:18:54 -0500

    UPDATE Login to use userRole to log users

--- src/controllers/AccessToken/login.js
+++ src/controllers/AccessToken/login.js
@@ -1,24 +1,65 @@
-const EmailAuthenticator = require('../../handlers/Auth/EmailAuthenticator')
-const EmailPasswordAuthenticator = require('../../handlers/Auth/EmailPasswordAuthenticator')
+const { User, UserRol } = require('../../db')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 module.exports = async (req, res) => {
     try {
-        const {
-            email,
-            password
-        } = req.body
-        // Use authenticator
+        const { email, password } = req.body
+
+        // Validar correo
+        if (!email)
+            return res.status(400).json({ error: 'Debe ingresar una email' })
+
+        // * Obtener usuario
+        const options = {
+            where: { email },
+            include: [
+                {
+                    model: UserRol, // * Para generar el token
+                    attributes: ['rol'],
+                },
+            ],
+        }
+        const user = await User.findOne(options)
+
+        // Validar usuario existente
+        if (!user) {
+            return res
+                .status(400)
+                .json({ error: 'Datos de logeo incorrectos.' })
+        }
+
+        // * Validar password (si es requerido). Por defecto es requerido
         const usePassword = req.body.usePassword !== false
-        const {
-            error,
-            token, auth, data
-        } = usePassword
-            ? await EmailPasswordAuthenticator(email, password)
-            : await EmailAuthenticator(email)
-        if (error) return res.status(400).json({ error })
+        if (usePassword) {
+            if (!password)
+                return res
+                    .status(400)
+                    .json({ error: 'Debe ingresar una password' })
+
+            // * Valida que la password sea correcta
+            const isValidPassword = await bcrypt.compare(
+                password,
+                user.password,
+            )
+
+            if (!isValidPassword)
+                return res
+                    .status(400)
+                    .json({ error: 'Datos de logeo incorrectos.' })
+        }
+
+        // * Generar token de la sesión
+        const token = jwt.sign(
+            { id: user.id, role: user.UserRol.rol },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: 60 * 60 * 24 * 6,
+            },
+        )
 
         // Return Response
-        res.status(201).json({ auth, token, data })
+        res.status(201).json({ auth: true, token, user })
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: error.message })
