407176cbdd0a20719c616704b27dc59842ffb463
Luis Mesajil <lhmesajil@gmail.com>
2024-01-09 12:40:47 -0500

    ADD route protection to getUsers ctrl allowing access to Admins

--- src/controllers/UserStatus/getStatuses.js
+++ src/controllers/UserStatus/getStatuses.js
@@ -1,14 +1,11 @@
 const { UserStatus } = require('../../db')
 
-const get = async(req, res) => {
-    
-    try{
+module.exports = async (req, res) => {
+    try {
         const data = await UserStatus.findAll()
         return res.status(200).json(data)
-    } catch(error){
-        console.error(error);
+    } catch (error) {
+        console.error(error)
         return res.status(500).json({ error: error.message })
     }
 }
-
-module.exports = get;
