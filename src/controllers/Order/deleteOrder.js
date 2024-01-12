f5a02ec62d703a7286380bb40406b7ee3c5a0a71
Luis Mesajil <lhmesajil@gmail.com>
2024-01-12 12:26:47 -0500

    UPDATE deleteOrder to set deletedAt | UPDATE prettier config to set printWidth

--- src/controllers/Order/deleteOrder.js
+++ src/controllers/Order/deleteOrder.js
@@ -1,26 +1,25 @@
-const { Order } = require('../../db');
+const { Order } = require('../../db')
 
 const deleteOrder = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { id } = req.params
 
         // Verificar que la orden existe
-        const order = await Order.findByPk(id);
+        const order = await Order.findByPk(id)
+
         if (!order) {
-            return res.status(404).json({ error: 'Order not found' });
+            return res.status(404).json({ error: 'Order no encontrada' })
         }
 
-        // Eliminar la orden
-        await Order.destroy({
-            where: {
-                id: id
-            }
-        });
+        // Eliminar la order
+        await order.update({ deletedAt: new Date() })
 
-        return res.status(200).json({ message: 'Order deleted successfully' });
+        return res.status(200).json({ message: 'Order eliminada exitosamente' })
     } catch (error) {
-        return res.status(500).json({ error: `There was an error processing your request : ${error.message}` });
+        // Revertir la transacción en caso de error
+        console.log(error)
+        return res.status(500).json({ error: error.message })
     }
-};
+}
 
-module.exports = deleteOrder;
+module.exports = deleteOrder
