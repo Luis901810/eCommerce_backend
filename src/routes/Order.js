b31fdbd1bad596ae6bc4cc17dffffa9ee910f940
Kevin_Padilla <kevin0696s@gmail.com>
2024-01-11 21:18:40 -0400

    Enpoint to get order by id added

--- src/routes/Order.js
+++ src/routes/Order.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', require('../controllers/Order/getOrder'))
+router.get('/:id', require('../controllers/Order/getOrderById'))
 router.post('/', require('../controllers/Order/createOrder'))
 router.put('/:id', require('../controllers/Order/updateOrder'))
 router.delete('/:id', require('../controllers/Order/deleteOrder'))
