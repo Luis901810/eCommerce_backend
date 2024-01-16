f28f4ce6480a7445565110d9390a27db32738085
Kevin_Padilla <kevin0696s@gmail.com>
2024-01-15 22:56:04 -0400

    Updating stock when the status of a order changes to Approved

--- src/controllers/Order/updateOrder.js
+++ src/controllers/Order/updateOrder.js
@@ -1,4 +1,5 @@
-const { Order } = require('../../db')
+const { Sequelize } = require('sequelize')
+const { Order, OrderLine, Shoe } = require('../../db')
 
 const updateOrder = async (req, res) => {
     try {
@@ -6,11 +7,48 @@ const updateOrder = async (req, res) => {
         const { totalAmount, statusId } = req.body //! PIDO el statusID
 
         // Verificar que la orden existe
-        const order = await Order.findByPk(id)
+        const order = await Order.findByPk(id, {
+            include: [
+                {
+                    model: OrderLine,
+                    include: [
+                        {
+                            model: Shoe,
+                            attributes: ['id', 'name', 'price'],
+                        },
+                    ],
+                },
+            ],
+        })
         if (!order) {
             return res.status(404).json({ error: 'Order not found' })
         }
 
+        if (statusId === '4567ffd5-8879-4455-9b06-b1cd23d3d0be') {
+            Promise.all(
+                order.OrderLines.map(async element => {
+                    const { shoeId, quantity } = element
+                    try {
+                        console.log(shoeId, quantity)
+                        const result = await Shoe.update(
+                            {
+                                stock: Sequelize.literal(`stock - ${quantity}`),
+                            },
+                            {
+                                where: { id: shoeId },
+                            },
+                        )
+                    } catch (error) {
+                        return res.status(400).json({ error: `There was an error updating product: ${shoeId}` })
+                    }
+                }),
+            ).then(() => {
+                
+              })
+              .catch((error) => {
+                return res.status(400).json({ error: `There was an error updating the OrderLine` })
+              })
+        }
         // Actualizar la orden
         await Order.update(
             { totalAmount, statusId }, //! PASO statusID
