6ee7e21b4435a8656832fc5c1c76aa6bfd55a1df
Sebastian Kang <sebi0522@gmail.com>
2024-01-16 20:51:46 -0300

    emails

---
+++ src/utils/email/constants.js
@@ -0,0 +1,9 @@
+const ID_PENDING = '4d17fad4-36a7-4225-8d0e-4f27a1a76b91'
+const ID_APPROVED = '9f88b2aa-5174-42c6-a5a7-5d9933dd7ef2'
+const ID_REJECTED = '0343ad31-d79f-4421-98dc-a31c3105ffb1'
+
+module.exports = {
+    ID_PENDING,
+    ID_APPROVED,
+    ID_REJECTED,
+}
--- src/utils/welcomeMail.js
+++
@@ -1,9 +0,0 @@
-const generateWelcomeMessage = userName => `
-    <div style="font-family: Arial, sans-serif; background-color: #f5f5f5; padding: 20px; border-radius: 10px; max-width: 600px; margin: 0 auto;">
-        <h1 style="color: #333;">Bienvenido a nuestro sitio. Gracias por registrarte, ${userName}!</h1>
-        <p style="color: #555; font-size: 16px;">Gracias por registrarte en nuestro increíble sitio. Estamos emocionados de tenerte con nosotros.</p>
-        <p style="color: #555; font-size: 16px;">¡Esperamos que disfrutes de tu tiempo en nuestra plataforma!</p>
-    </div>
-`
-
-module.exports = { generateWelcomeMessage }
